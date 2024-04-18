606c880f99496c7ad4b9cd0f7987fb6eff68e110
Strahinja Sekulic <strahinja0123@gmail.com>
2024-04-18 17:40:55 +0200

    Filter: Subjects

--- filtering.py
+++ filtering.py
@@ -1,30 +1,52 @@
 import json
-import string
-from random import random
+import re
+
+import enchant
 
 common_fields = []
 removed_fields = []
+english = enchant.Dict("en_US")
 
 
 def write_all(data):
-    with open('./even_more_filtered_books.txt', 'w') as f:
+    with open('./even_more_filtered_books_lower.txt', 'w') as f:
         f.writelines(data)
 
 # ['subjects', 'key', 'title', 'authors', 'type', 'latest_revision', 'revision', 'created', 'last_modified']
-# ['covers', 'subject_places', 'subject_people', 'subject_times', 'location']
+# ['location']
 
 
 def print_filtered():
-    with open('./even_more_filtered_books.txt', 'r') as f:
+    with open('./even_more_filtered_books_lower.txt', 'r') as f:
         for line in f.readlines():
             print(json.dumps(json.loads(line), indent=4))
             print('---------------------------------------------------------------------')
 
 
+def filter_subjects(subjects):
+    filtered = []
+    for subject in subjects:
+        if not re.search("^[A-Za-z\s]+$", subject):
+           continue
+        if not str.isascii(subject):
+           continue
+        for word in subject.split(" "):
+           if len(word) and not english.check(word):
+               break
+        else:
+           filtered.append(subject.lower())
+    return filtered
+
+max_lines = 0
+curr_line = 1
+
 def main():
+    global curr_line, max_lines
     fixed = []
-    with open('./filtered_books.txt', 'r') as f:
-        for line in f.readlines():
+    with (open('./filtered_books.txt', 'r') as f):
+        lines = f.readlines()
+        max_lines = len(lines)
+        for line in lines:
             # print(json.dumps(json.loads(line), indent=4))
             # print('---------------------------------------------------------------------')
             try:
@@ -97,6 +119,20 @@ def main():
             # type
             if 'type' in book.keys(): book.pop('type')
 
+            # subject_places
+            if 'subject_places' in book.keys(): book.pop('subject_places')
+
+            # subject_people
+            if 'subject_people' in book.keys(): book.pop('subject_people')
+
+            # subject_times
+            if 'subject_times' in book.keys(): book.pop('subject_times')
+
+            # subjects
+
+            filtered_subjects = filter_subjects(book['subjects'])
+            book['subjects'] = filtered_subjects
+
             # first_publish_date
             if 'first_publish_date' in book.keys():
                 year = book['first_publish_date'].split(',')[-1].split(' ')[-1]
@@ -114,6 +150,10 @@ def main():
             if 'location' not in book.keys(): book['location'] = ""
 
             fixed.append(json.dumps(book) + '\n')
+            curr_line += 1
+            if (curr_line % 10000 == 0):
+                print(f"{curr_line}/{max_lines}")
+
 
     print(f'Common Fields: {common_fields}')
     write_all(fixed)

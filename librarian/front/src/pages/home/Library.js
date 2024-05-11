902236db36711a2735a769acf6cab031ef6ec812
Strahinja Sekulic <strahinja0123@gmail.com>
2024-05-11 17:55:11 +0200

    Add: Redesigned color scheme

--- librarian/front/src/pages/home/Library.js
+++ librarian/front/src/pages/home/Library.js
@@ -3,9 +3,23 @@ export default function LibraryPage() {
     const token = localStorage.getItem("token")
     const email = localStorage.getItem("username")
 
+    const handleLogout = () => { window.location.href = "/" }
+
     return(
         <main>
-            <p>Yey! {email}</p>
+            <div className="w-100 standard-padding-xs center" style={{display:'grid', gridTemplateColumns:'0.2fr 2fr 0.2fr', justifyContent:'end'}}>
+                <p className="hero-title"><b>Librari</b>an</p>
+                <div className="flex justify-center gap-l">
+                    <button className="text-button text-button-selected">My Library</button>
+                    <button className="text-button">Trending</button>
+                    <button className="text-button">Recommend</button>
+                </div>
+                <div className="flex center justify-end">
+                    <button className="icon-button">
+                        <span className="material-symbols-outlined icon" onClick={handleLogout}>logout</span>
+                    </button>
+                </div>
+            </div>
         </main>
     )
 }

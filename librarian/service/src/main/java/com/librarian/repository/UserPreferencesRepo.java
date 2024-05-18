35b59357682d97e61efd1267a984babd2677f930
Strahinja Sekulic <strahinja0123@gmail.com>
2024-05-18 09:26:30 +0200

    Fix: SQL Fetching UP

--- librarian/service/src/main/java/com/librarian/repository/UserPreferencesRepo.java
+++ librarian/service/src/main/java/com/librarian/repository/UserPreferencesRepo.java
@@ -1,12 +1,19 @@
 package com.librarian.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.librarian.model.UserPreferences;
 
 @Repository
 public interface UserPreferencesRepo extends JpaRepository<UserPreferences, Long> {
+
     Optional<UserPreferences> findById(Long id);
+
+    @Query("SELECT DISTINCT prefs FROM UserPreferences prefs JOIN FETCH prefs.library book JOIN FETCH book.subjects JOIN FETCH book.authors WHERE prefs.id = :id")
+    List<UserPreferences> findAllById(@Param("id") Long id);
 }
--- librarian/service/src/main/java/com/librarian/service/UserPreferencesService.java
+++ librarian/service/src/main/java/com/librarian/service/UserPreferencesService.java
@@ -2,6 +2,7 @@ package com.librarian.service;
 
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpResponseException;
+import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,8 @@ public class UserPreferencesService {
     @Autowired 
     private  IUserRepository userRepo;
     @Autowired
+    private UserPreferencesRepo userPreferencesRepo;
+    @Autowired
     private ReadBookRepo readBookRepo;
     @Autowired
     private BooksRepo booksRepo;
@@ -41,10 +44,22 @@ public class UserPreferencesService {
     Logger logger = LoggerFactory.getLogger(UserPreferencesService.class);
 
     private UserPreferences _get(String username) {
+        logger.info("Fetching the user with username: " + username);
         User user = userRepo.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
-        UserPreferences prefs = user.getPreferences();
-        prefs.getLibrary();
-        return user.getPreferences();
+        return _get(user.getPreferences().getId());
+    }
+    private UserPreferences _get(Long id) {
+        try {
+            UserPreferences prefs = userPreferencesRepo.findAllById(id).get(0);
+            logger.debug("Found items of size: " + prefs.getLibrary().size() + ":", prefs.getLibrary());
+            return prefs;
+        }
+        catch(IndexOutOfBoundsException ex) {
+            throw new UsernameNotFoundException("User Preferences Not Found for id: " + id);
+        }
+    }
+    private UserPreferencesDTO _save(UserPreferences preferences) {
+        return new UserPreferencesDTO(_get(repo.save(preferences).getId()));
     }
 
     public UserPreferencesDTO get(String username) {
@@ -55,58 +70,59 @@ public class UserPreferencesService {
         UserPreferences preferences = _get(username);
         preferences.setAge(age);
         preferences.setTargetYear(targetYear);
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 
     public UserPreferencesDTO addAdditionalSubject(String username, Long subjectId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         Subject subject = subjectsRepo.findById(subjectId).orElseThrow(() -> new HttpResponseException(HttpStatus.SC_NOT_FOUND, "Subject not found"));
         preferences.getAdditionalSubjects().add(subject);
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 
     public UserPreferencesDTO deleteAdditionalSubject(String username, Long subjectId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         preferences.getAdditionalSubjects().removeIf((item) -> item.getId().equals(subjectId));
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 
     public UserPreferencesDTO addLikedSubject(String username, Long subjectId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         Subject subject = subjectsRepo.findById(subjectId).orElseThrow(() -> new HttpResponseException(HttpStatus.SC_NOT_FOUND, "Subject not found"));
-        preferences.getLikedSubjects().add(subject);
-        return new UserPreferencesDTO(repo.save(preferences));
+        preferences.getLikedSubjects().add(subject);        
+        return _save(preferences);
+
     }
 
     public UserPreferencesDTO deleteLikedSubject(String username, Long subjectId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         preferences.getLikedSubjects().removeIf((item) -> item.getId().equals(subjectId));
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 
     public UserPreferencesDTO addLikedAuthor(String username, Long authorId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         Author author = authorsRepo.findById(authorId).orElseThrow(() -> new HttpResponseException(HttpStatus.SC_NOT_FOUND, "Author not found"));
         preferences.getLikedAuthors().add(author);
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 
     public UserPreferencesDTO deleteLikedAuthor(String username, Long authorId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         preferences.getLikedAuthors().removeIf((item) -> item.getId().equals(authorId));
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 
     public UserPreferencesDTO addBookToLibrary(String username, Long bookId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         Book book = booksRepo.findById(bookId).orElseThrow(() -> new HttpResponseException(HttpStatus.SC_NOT_FOUND, "Book not found"));
         preferences.getLibrary().add(book);
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 
     public UserPreferencesDTO removeBookFromLibrary(String username, Long bookId) throws HttpResponseException {
         UserPreferences preferences = _get(username);
         preferences.getLibrary().removeIf((item) -> item.getId().equals(bookId));
-        return new UserPreferencesDTO(repo.save(preferences));
+        return _save(preferences);
     }
 }
